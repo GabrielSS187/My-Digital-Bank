2df64b1b929b32f5273319102ab7a94660bde42f
Gabriel Silva <gabriel127.gk@gmail.com>
2024-05-31 16:58:23 -0300

    config: add algumas configurações.

---
+++ src/app/(auth)/signUp/page.tsx
@@ -0,0 +1,9 @@
+import { FormRegisterAndLogin } from '@/components/FormRegisterAndLogin';
+
+export default function Page() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <FormRegisterAndLogin isFormLogin={false} />
+    </main>
+  );
+}
