eba4c9c10d8fa606b6d01e6faf778bef322c292a
Gabriel Silva <gabriel127.gk@gmail.com>
2024-06-09 16:03:46 -0300

    git changes: rm package-lock.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,7 @@ import { useEffect } from 'react';
 export default function Home() {
   const router = useRouter();
 
-  useEffect(() => {
-    router.push('/signIn');
-  }, []);
+  
 
   return <></>;
 }
